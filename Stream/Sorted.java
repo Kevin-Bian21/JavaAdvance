a8e6e47b2c3e5a8a0d58c29d09764fc157b800fe
BianWenKai <1467295546@qq.com>
2021-11-30 17:15:24 +0800

    Stream: Sorted 排序

---
+++ Stream/Sorted.java
@@ -0,0 +1,24 @@
+package Stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author BianWenKai
+ * @DATE 2021/11/30 - 16:40
+ **/
+public class Sorted {
+    public static void main(String[] args) {
+        Person p1 = new Person("Jone",15);
+        Person p2 = new Person("Bob",22);
+        Person p3 = new Person("Jack",13);
+        Person p4 = new Person("Tom",18);
+        List<Person> persons = new ArrayList<>(Arrays.asList(p1, p2, p3, p4));
+
+        persons.stream()
+                .sorted((o1, o2) -> o1.getAge() - o2.getAge())
+                .forEach(x -> System.out.println(x.getName()));
+    }
+}
